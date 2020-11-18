a65490a913f2bfe1b1e6b114255a24371d243ad1
Franco <Franco Gomez>
2020-11-18 12:29:34 -0300

    Design almost finished

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,24 +1,49 @@
-import React, { Component } from "react"
+import React from "react"
 import BoardList from "./board-list/BoardList"
+import profile from "../../assets/images/profile.png"
 import "./Board.scss"
 
-class Board extends Component {
-  constructor() {
-    super()
-    this.state = {
-      planningList: [],
-    }
-  }
-
-  render() {
-    return (
-      <div className="Board__wrapper">
-        <div>
-          <BoardList />
-        </div>
+const Board = () => {
+  return (
+    <div className="Board__wrapper">
+      <div>
+        <header className="header">
+          <div className="project__title">
+            <h1>Support and Improvements</h1>
+            <div className="project__users">
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+              <img src={profile} alt="" width="26px" height="26px" />
+            </div>
+          </div>
+          <nav className="nav">
+            <ul>
+              <li>Board</li>
+              <li>Gant</li>
+              <li>Activity</li>
+              <li>Checklists</li>
+              <li>Files</li>
+              <li>Details</li>
+            </ul>
+            <div className="options">
+              <div className="option__button">
+                <p>Add filter</p>
+              </div>
+              <div className="option__button">
+                <p>Group by</p>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <BoardList />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Board
--- src/components/board/board-list/Subcomponents/task/Task.jsx
+++ src/components/board/board-list/Subcomponents/task/Task.jsx
@@ -3,6 +3,7 @@ import Calendar from "../../../../html/SVG/Calendar"
 import Chat from "../../../../html/SVG/Chat"
 import File from "../../../../html/SVG/File"
 import Menu from "../../../../html/SVG/Menu"
+import profile from "../../../../../assets/images/profile.png"
 import "./Task.scss"
 
 const Task = ({item, handleDragStart, activeClass, handleDragOverItem}) => (
@@ -25,7 +26,7 @@ const Task = ({item, handleDragStart, activeClass, handleDragOverItem}) => (
       </div>
       <div className="button__wrapper">
         <Chat />
-        <p>4</p>
+        <p>6</p>
       </div>
       <div className="button__wrapper">
         <Calendar />
@@ -34,8 +35,10 @@ const Task = ({item, handleDragStart, activeClass, handleDragOverItem}) => (
     </div>
     </div>
     <div className="footer__wrapper">
-      <p>Discovery</p>
-      <img src="" alt=""/>
+      <p className="label">Discovery</p>
+      <div className="people">
+        <img src={profile} width="26px" height="26px" alt="user" />
+      </div>
     </div>
   </div>
 )
