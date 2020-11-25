77ab082169d9750870016a66a553af4e8f860513
Franco <Franco Gomez>
2020-11-25 18:14:33 -0300

    Merged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import Routes from "./Routes";
 
 function App() {
-    return (
-        <div className="App">
-            <Routes/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
