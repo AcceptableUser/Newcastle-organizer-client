f2f9769ff53fae9abbcabc19a5614757ff74c2ff
Franco <Franco Gomez>
2020-11-25 18:28:53 -0300

    Final merge changes added

--- src/components/boards/Boards.jsx
+++ src/components/boards/Boards.jsx
@@ -1,45 +1,13 @@
 import React from "react";
 import BoardsList from "./boards-list/BoardsList";
 import "./Boards.scss";
-<<<<<<< HEAD
-import profileGirl from "../../assets/images/profile-girl.png";
-=======
 import BoardsHeader from "./boards-header/BoardsHeader";
->>>>>>> 1b70475bc074628fdc48fcc25b2ff6d34203dbc3
 
 const Boards = () => {
   return (
     <div className="boards__wrapper">
-<<<<<<< HEAD
-      <header className="boards__header">
-        <nav className="boards__header__navigation">
-          <h3 className="boards__header__navigation__title">
-            Newcastle Organizer
-          </h3>
-          <button className="boards__header__navigation__user__options">
-            <img
-              className="boards__header__navigation__user__image"
-              src={profileGirl}
-              alt="porfile girl"
-            />
-          </button>
-        </nav>
-      </header>
-      <main className="boards__main">
-        <div className="boards__main__title__wrapper">
-          <h1 className="boards__main__title">Here you can see your boards</h1>
-          <p className="boards__main__description">
-            And also those others shared with you!
-          </p>
-        </div>
-        <BoardsList listName={"Recently Viewed"} />
-        <BoardsList listName={"Personal Boards"} />
-        <BoardsList listName={"Shared Boards"} />
-      </main>
-=======
       <BoardsHeader />
       <BoardsList />
->>>>>>> 1b70475bc074628fdc48fcc25b2ff6d34203dbc3
     </div>
   );
 };
--- src/components/boards/boards-list/BoardsList.jsx
+++ src/components/boards/boards-list/BoardsList.jsx
@@ -1,45 +1,5 @@
 import React from "react";
 import "./BoardsList.scss";
-<<<<<<< HEAD
-import boardBackground1 from "../../../assets/images/board-background1.jpg";
-import boardBackground2 from "../../../assets/images/board-background2.jpg";
-import boardBackground3 from "../../../assets/images/board-background3.jpg";
-import boardBackground4 from "../../../assets/images/board-background4.jpg";
-
-const BoardsList = ({ listName }) => {
-  return (
-    <div className="boards-list__section__wrapper">
-      <section className="boards-list__section">
-        <h2 className="boards-list__section__title">{listName}</h2>
-        <ul className="boards-list__section__list">
-          <li className="boards-list__section__list__item">
-            <img
-              className="boards-list__section__list__item__image"
-              src={boardBackground1}
-              alt=""
-            />
-          </li>
-          <li className="boards-list__section__list__item">
-            <img
-              className="boards-list__section__list__item__image"
-              src={boardBackground2}
-              alt=""
-            />
-          </li>
-          <li className="boards-list__section__list__item">
-            <img
-              className="boards-list__section__list__item__image"
-              src={boardBackground3}
-              alt=""
-            />
-          </li>
-          <li className="boards-list__section__list__item">
-            <img
-              className="boards-list__section__list__item__image"
-              src={boardBackground4}
-              alt=""
-            />
-=======
 import NormalBoard from "./Subcomponents/normal-board/NormalBoard";
 import RecentBoard from "./Subcomponents/recent-board/RecentBoard";
 
@@ -117,7 +77,6 @@ const BoardsList = () => {
           </li>
           <li className="board-list__section__list__item">
             <NormalBoard />
->>>>>>> 1b70475bc074628fdc48fcc25b2ff6d34203dbc3
           </li>
         </ul>
       </section>
