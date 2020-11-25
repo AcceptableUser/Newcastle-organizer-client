9d26f34825bf71968660293c67fbcf6bfb2342b2
Franco <Franco Gomez>
2020-11-25 19:02:42 -0300

    Minor error fixed

--- src/components/boards/Boards.jsx
+++ src/components/boards/Boards.jsx
@@ -1,52 +1,13 @@
 import React from "react";
 import BoardsList from "./boards-list/BoardsList";
 import "./Boards.scss";
-<<<<<<< HEAD
 import BoardsHeader from "./boards-header/BoardsHeader";
-=======
-import profileGirl from "../../assets/images/profile-girl.png";
-import Loupe from "../html/SVG/Loupe";
->>>>>>> Changes in the boards section
 
 const Boards = () => {
   return (
     <div className="boards__wrapper">
-<<<<<<< HEAD
       <BoardsHeader />
       <BoardsList />
-=======
-      <header className="boards__header">
-        <nav className="boards__header__navigation">
-          <h3 className="boards__header__navigation__title">
-            Newcastle Organizer
-          </h3>
-          <button className="boards__header__navigation__user__options">
-            <img
-              className="boards__header__navigation__user__image"
-              src={profileGirl}
-              alt="porfile girl"
-            />
-          </button>
-        </nav>
-      </header>
-      <main className="boards__main">
-        <div className="boards__main__searchbar">
-          <input className="boards__main__searchbar__input" type="text" />
-          <button className="boards__main__searchbar__button">
-            <Loupe />
-          </button>
-        </div>
-        <div className="boards__main__title__wrapper">
-          <h1 className="boards__main__title">Here you can see your boards</h1>
-          <p className="boards__main__description">
-            And also those others shared with you!
-          </p>
-        </div>
-        <BoardsList listName={"Recently Viewed"} number={1} />
-        <BoardsList listName={"Personal Boards"} number={5} />
-        <BoardsList listName={"Shared Boards"} number={50} />
-      </main>
->>>>>>> Changes in the boards section
     </div>
   );
 };
--- src/components/boards/boards-list/BoardsList.jsx
+++ src/components/boards/boards-list/BoardsList.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./BoardsList.scss";
-<<<<<<< HEAD
 import NormalBoard from "./Subcomponents/normal-board/NormalBoard";
 import RecentBoard from "./Subcomponents/recent-board/RecentBoard";
 
@@ -80,59 +79,6 @@ const BoardsList = () => {
             <NormalBoard />
           </li>
         </ul>
-=======
-
-const BoardsList = ({ listName, number }) => {
-  return (
-    <div className="boards-list__section__wrapper">
-      <section className="boards-list__section">
-        <h2 className="boards-list__section__title">{listName}</h2>
-        <div className="boards-list__section__list__wrapper">
-          <ul className="boards-list__section__list">
-            <li
-              className="boards-list__section__list__item"
-              style={{
-                backgroundImage:
-                  "url(https://source.unsplash.com/350x200/?" +
-                  number * 2 +
-                  ",plant)",
-              }}
-            ></li>
-            <li
-              className="boards-list__section__list__item"
-              style={{
-                backgroundImage:
-                  "url(https://source.unsplash.com/500x400/?" +
-                  number * 5 +
-                  ",plant)",
-              }}
-            ></li>
-            <li
-              className="boards-list__section__list__item"
-              style={{
-                backgroundImage:
-                  "url(https://source.unsplash.com/500x400/?" +
-                  number * 9 +
-                  ",plant)",
-              }}
-            ></li>
-            <li
-              className="boards-list__section__list__item"
-              style={{
-                backgroundImage:
-                  "url(https://source.unsplash.com/500x400/?" +
-                  number * 14 +
-                  ",plant)",
-              }}
-            ></li>
-          </ul>
-          <input
-            className="boards-list__section__list__button"
-            type="button"
-            value="Load more boards"
-          />
-        </div>
->>>>>>> Changes in the boards section
       </section>
     </div>
   );
