1804f278c71cbffb14733e45c725ac70efedf059
Franco <Franco Gomez>
2020-11-25 19:06:24 -0300

    Minor error fixed

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -5,7 +5,7 @@ import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 import UserPage from "../pages/UserPage";
 import BoardPage from "../pages/BoardPage";
-import ProjectPage from "../pages/ProjectPage";
+// import ProjectPage from "../pages/ProjectPage";
 import BoardsPage from "../pages/BoardsPage";
 
 function Routes() {
@@ -15,9 +15,9 @@ function Routes() {
         <Route path="/login" exact component={LoginPage} />
         <Route path="/signup" exact component={SignupPage} />
         <Route path="/user" exact component={UserPage} />
-        <Route path="/projects/:id" exact component={BoardPage} />
+        <Route path="/boards/:id" exact component={BoardPage} />
         <Route path="/boards" exact component={BoardsPage} />
-        <Route path="/projects" exact component={ProjectPage} />
+        {/* <Route path="/projects" exact component={ProjectPage} /> */}
       </Switch>
     </Router>
   );
